794567f7445529892c2548f900fffa0081180b2f
bahautdinov_bahtiyar <sabakilai@gmail.com>
2017-08-17 12:02:25 +0600

    init

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,10 @@ let getQuestion = require('../libs/getQuestion');
 var Sequelize = require("sequelize");
 var sequelize = new Sequelize("ddg6gen02vnjki", "vibhiihldjoeks", "9838b357de38135b867180590c12619c41949bca0a39a866072fc16126d7e862", {
 	host: "ec2-23-21-220-48.compute-1.amazonaws.com",
-	dialect: "postgres"
+	dialect: "postgres",
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 
